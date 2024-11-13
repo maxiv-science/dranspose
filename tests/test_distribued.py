a3e095dce4cb7413f66ada6e8da1f60f978f40e6
Michele Cascella <michele.cascella@maxiv.lu.se>
2024-11-13 13:27:55 +0100

    test lobotomised worker

--- tests/test_distribued.py
+++ tests/test_distribued.py
@@ -1,9 +1,11 @@
 import asyncio
 import json
+from uuid import UUID
 
+from pydantic import UUID4
 import pytest
 from dranspose.mapping import MappingSequence
-from dranspose.helpers.utils import parameters_hash
+from dranspose.helpers.utils import parameters_hash, done_callback, cancel_and_wait
 
 from dranspose.protocol import (
     GENERIC_WORKER,
@@ -13,14 +15,19 @@ from dranspose.protocol import (
     WorkParameter,
     WorkerName,
 )
-from dranspose.helpers.utils import done_callback
 
 import logging
 import redis.asyncio as redis
 from redis import Redis
 
 from dranspose.distributed import DistributedService, DistributedSettings
-from dranspose.worker import WorkerState
+from dranspose.worker import WorkerState, Worker, WorkerSettings
+import zmq
+import zmq.asyncio
+
+from dranspose.protocol import IngesterState, ConnectedWorker, StreamName
+
+from dranspose.ingester import IngesterSettings
 
 
 class DummyWorker(DistributedService):
@@ -119,7 +126,7 @@ async def test_distributed() -> None:
     assert json.loads(worker_json[0])["name"] == "dummy"
     assert json.loads(worker_json[0])["parameters_hash"] is None
 
-    # what if we publish a new map?
+    # what if we publish a map?
     await publish_controller_update(rds)
     await asyncio.sleep(1.5)
     # now the dummy parameters should be {} and have a corresponding hash
@@ -139,5 +146,114 @@ async def test_distributed() -> None:
     await dummy.close()
     await cleanup_redis(rds)
 
+    # leave time to the redis poll task to update
+    await asyncio.sleep(0.2)
+
+
+class IngestOnlyWorker(Worker):
+    async def run(self) -> None:
+        self.manage_ingester_task = asyncio.create_task(self.manage_ingesters())
+        self.manage_ingester_task.add_done_callback(done_callback)
+        await self.register()
+
+    async def restart_work(self, uuid: UUID4, active_streams: list[StreamName]) -> None:
+        pass
+
+    async def close(self) -> None:
+        await cancel_and_wait(self.manage_ingester_task)
+        if self.dequeue_task is not None:
+            await cancel_and_wait(self.dequeue_task)
+        await self.redis.delete(RedisKeys.config("worker", self.state.name))
+        # await super().close() # this will try to cancel non existing coro
+        # I'll try to do the cleanup by hand
+        await self.redis.aclose()
+        await self.raw_redis.aclose()
+        self.ctx.destroy()
+        self._logger.info("worker closed")
+
+
+@pytest.mark.asyncio
+async def test_ingest_only_worker() -> None:
+    dummy = IngestOnlyWorker(WorkerSettings(worker_name="dummy"))
+
+    rds: Redis = redis.from_url(
+        f"{dummy._distributed_settings.redis_dsn}?decode_responses=True&protocol=3"
+    )
+    # let's clean up the DB in case a previous test failed
+    await cleanup_redis(rds)
+
+    # start the dummy worker
+    t = asyncio.create_task(dummy.run())
+    t.add_done_callback(done_callback)
+    await asyncio.sleep(1.5)
+    # test that dummy has published its config
+    worker_json = await get_published_worker_congig(rds)
+    assert json.loads(worker_json[0])["name"] == "dummy"
+    assert json.loads(worker_json[0])["parameters_hash"] is None
+
+    # what if we publish a new map?
+    await publish_controller_update(rds)
+    await asyncio.sleep(1.5)
+    # now the dummy parameters should be {} and have a corresponding hash
+    worker_json = await get_published_worker_congig(rds)
+    assert json.loads(worker_json[0])["parameters_hash"] == parameters_hash({})
+
+    # Create a state/settings for our fake ingester
+    # iport = 10000
+    isettings = IngesterSettings(
+        ingester_name="fake_ingester",
+        ingester_streams=["fake_stream"],
+        # ingester_url = f"tcp://localhost:{iport}",
+    )
+    istate = IngesterState(
+        name=isettings.ingester_name,
+        url=isettings.ingester_url,
+        streams=isettings.ingester_streams,
+    )
+
+    # create a dummy ingester either a dumb ROUTER socket or spin up a full ingester
+    ctx = zmq.asyncio.Context()
+    out_socket = ctx.socket(zmq.ROUTER)
+    out_socket.setsockopt(zmq.ROUTER_MANDATORY, 1)
+    out_socket.bind(f"tcp://*:{isettings.ingester_url.port}")
+
+    # publish ingester key/state
+    # this is what the worker looks for:
+    # configs = await self.redis.keys(RedisKeys.config("ingester"))
+    # who publishes it, the controller or the ingester?
+    # I think it's the ingester with publish_config()
+    async with rds.pipeline() as pipe:
+        await pipe.setex(
+            RedisKeys.config("ingester", istate.name),
+            10,
+            istate.model_dump_json(),
+        )
+        await pipe.execute()
+
+    await asyncio.sleep(1.5)
+
+    # accept the worker
+    # verify that worker has sent its identity to the ingester
+    poller = zmq.asyncio.Poller()
+    poller.register(out_socket, zmq.POLLIN)
+    socks = dict(await poller.poll(timeout=1))
+    for sock in socks:
+        data = await sock.recv_multipart()
+        connected_worker = ConnectedWorker(
+            name=data[0], service_uuid=UUID(bytes=data[1])
+        )
+        logging.info("worker pinnged %s", connected_worker)
+
+    # verify that worker has picked up the ingester, how?
+    logging.info(f"{dummy._ingesters=}")
+    assert len(dummy._ingesters) > 0
+
+    # one can also look at the status published on redis
+
+    # change service_uuid and verify that worker drops ingester
+
+    await dummy.close()
+    await cleanup_redis(rds)
+    ctx.destroy(linger=0)
     # leave time to the redis poll task to update
     await asyncio.sleep(0.2)
